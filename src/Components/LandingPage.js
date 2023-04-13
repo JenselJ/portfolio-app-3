0900e8fe910245989284788b51231cf445da69f1
JenselJ <saneer2002@gmail.com>
2023-04-13 11:00:34 +1000

    added border corners

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,17 @@
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="base-div p-4">
+      <div className="triangle-parent">
+        <div className="triangle-top-left"></div>
+        <div className="triangle-bottom-right"></div>
+      </div>
+      <div className="main-div p-12">
+        <div className="main-div-child"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
