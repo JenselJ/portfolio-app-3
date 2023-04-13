1a17f4333d7b362b341b79aa61922fe9bf081f93
JenselJ <saneer2002@gmail.com>
2023-04-13 11:23:13 +1000

    adding nav bar

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -8,7 +8,19 @@ const LandingPage = () => {
         <div className="triangle-bottom-right"></div>
       </div>
       <div className="main-div p-12">
-        <div className="main-div-child"></div>
+        <div className="main-div-child">
+          <div className="nav-div h-16 border border-red-500 flex justify-between px-4 pt-3">
+            <div className="text-aqua">Sunny</div>
+            <div className="text-gray flex">
+              <div className="mr-8">
+                <a className="text-aqua">1.</a> Home
+              </div>
+              <div className="mr-8">2. About</div>
+              <div className="mr-8">3. Projects</div>
+              <div>4. Contact</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        aqua: "#59ffd5",
+        gray: "#c5d0f5",
+      },
+    },
   },
   plugins: [],
 };
